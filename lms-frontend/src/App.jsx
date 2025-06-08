c19592fd1b755e6778f3ccbeba764ab24d62fb99
HarshPatel0523 <harshpatelbxn@gmail.com>
2025-06-08 15:13:37 +0530

    Added Not Found Page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 import AboutUs from "./Pages/AboutUs"
+import NotFound from "./Pages/NotFound"
 
 const App = () => {
   return <>
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/about" element={<AboutUs />}/>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   </>
 }
